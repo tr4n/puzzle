417627509cf8d83b812d17093426c08249a30575
huytq <huytq@ominext.com>
2022-08-31 17:52:03 +0700

    validate move

--- app/src/main/java/com/tr4n/puzzle/data/model/State.kt
+++ app/src/main/java/com/tr4n/puzzle/data/model/State.kt
@@ -14,15 +14,16 @@ data class State(val puzzles: List<Puzzle>) {
 
     fun move(move: Move): State {
         val currentEmptyIndex = puzzles.indexOfFirst { it.index == 0 }.takeIf { it > 0 } ?: 0
-        val delta = when (move) {
-            Move.LEFT -> 1
-            Move.RIGHT -> -1
-            Move.UP -> size
-            Move.DOWN -> -size
-
+        val currentRow = currentEmptyIndex / size
+        val currentColumn = currentEmptyIndex % size
+        val delta = when {
+            move == Move.LEFT && currentColumn < size - 1 -> 1
+            move == Move.RIGHT && currentColumn > 0 -> -1
+            move == Move.UP && currentRow < size - 1 -> size
+            move == Move.DOWN && currentRow > 0 -> -size
+            else -> return this
         }
         val nextIndex = currentEmptyIndex + delta
-        if (nextIndex !in puzzles.indices) return this
 
         val newPuzzles = puzzles.mapIndexed { index, puzzle ->
             when (index) {
